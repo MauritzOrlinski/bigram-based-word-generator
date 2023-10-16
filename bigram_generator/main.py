0279f151454b8cd0e4d61cef211e438841a0a27d
MauritzOrlinski <mauorlin@gmail.com>
2023-10-16 10:59:17 +0200

    checking lowercase word

--- bigram_generator/main.py
+++ bigram_generator/main.py
@@ -4,11 +4,12 @@ from get_data import get_data as gd
 from nltk.corpus import words
 from sys import argv
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile
 import pathlib
 
+
 def main():
-    data_dir = pathlib.Path(__file__).parent.parent / 'data'
+    data_dir = pathlib.Path(__file__).parent.parent / "data"
     try:
         number_of_words_to_generate = int(argv[1])
     except IndexError:
@@ -22,24 +23,25 @@ def main():
     except IndexError:
         paths = [data_dir / f for f in listdir(data_dir) if isfile(data_dir / f)]
 
-    print(f"Paths: {paths}")
+    print(f"The used path: {paths}")
 
     print(f"Number of words to generate: {number_of_words_to_generate}")
 
     generator = bm.BigramWordGeneration()
     generator.train(gd.get_words_from_file(paths))
     number_of_real_words = 0
-    for i in range(number_of_words_to_generate):
+    for _ in range(number_of_words_to_generate):
         generated_word = generator.generate()
-        is_real_word = generated_word in words.words()
+        is_real_word = generated_word.lower() in words.words()
         print(f"{generated_word} is a word: {is_real_word}")
         if is_real_word:
             number_of_real_words += 1
 
     print(
-        f"Number of real words: {number_of_real_words} \nNumber of generated words: {number_of_words_to_generate} \nAccuracy: {number_of_real_words / number_of_words_to_generate}")
+        f"Number of real words: {number_of_real_words} \nNumber of generated words: {number_of_words_to_generate} \nAccuracy: {number_of_real_words / number_of_words_to_generate}"
+    )
 
 
-if __name__ == '__main__':
-    nltk.download('words')
+if __name__ == "__main__":
+    nltk.download("words")
     main()
